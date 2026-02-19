dab3f45b19513ba53eb6537ac3ad8da041841484
Krishna Prasad Prajapati <kshp97958@gmail.com>
2026-02-19 11:40:22 +0530

    Add main method to BaseClass for testing

--- SimpleMaven/src/main/java/genericUtility/BaseClass.java
+++ SimpleMaven/src/main/java/genericUtility/BaseClass.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 public class BaseClass {
 	
 	public static WebDriver driver;
+
+	public static void main(String[] args){
+		System.out.println("Main Start");
+	}
 	
 
 }
